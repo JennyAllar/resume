14a0433a697f9291c25406a394238dbd596449ec
Jenny Allar <JennyAllar@gmail.com>
2014-04-04 21:48:11 -0600

    Added middleman favicon gem and favicon.

--- config.rb
+++ config.rb
@@ -102,4 +102,12 @@ configure :build do
 
   # Or use a different image path
   # set :http_path, "/Content/images/"
+
+  activate :favicon_maker, :icons => {
+    "favicon_template.png" => [
+      { icon: "apple-touch-icon-152x152-precomposed.png" },
+      { icon: "apple-touch-icon-114x114-precomposed.png" },
+      { icon: "apple-touch-icon-72x72-precomposed.png" },
+    ]
+  }
 end
